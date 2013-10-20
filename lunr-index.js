4acbe625f04e1a0698b08e4d1a54e50bc46ad1b8
Emanuele Bardelli <bardellie@gmail.com>
2013-10-20 10:50:14 -0700

    Add index page

--- lunr-index.js
+++ lunr-index.js
@@ -8,6 +8,36 @@ var documentTitles = {};
 
 
 
+documentTitles["index.html#common-latex-service-interface"] = "Common LaTeX Service Interface";
+index.add({
+    url: "index.html#common-latex-service-interface",
+    title: "Common LaTeX Service Interface",
+    body: "# Common LaTeX Service Interface The Common LaTeX Service Interface (CLSI) is an HTTP RESTful API for compiling LaTeX documents online.  "
+});
+
+documentTitles["index.html#how-to-get-access"] = "How to get access";
+index.add({
+    url: "index.html#how-to-get-access",
+    title: "How to get access",
+    body: "## How to get access To access the CLSI server you will need a developer token. To request a token, please email the server maintainer.   "
+});
+
+documentTitles["index.html#how-to-report-a-missing-package"] = "How to report a missing package";
+index.add({
+    url: "index.html#how-to-report-a-missing-package",
+    title: "How to report a missing package",
+    body: "## How to report a missing package If the CLSI maintainer decided to run a striped-down version of LaTeX, it is possible that some packages are not installed by default. Please contact the CSLI maintainer for more information regarding missing packages. "
+});
+
+documentTitles["index.html#client"] = "Client";
+index.add({
+    url: "index.html#client",
+    title: "Client",
+    body: "## Client If you are looking for a client to access this CLSI server, check out the [Remote LaTeX](http://github.com/pacbard/RLatex) client. "
+});
+
+
+
 documentTitles["access.html#accessing-the-clsi"] = "Accessing the CLSI";
 index.add({
     url: "access.html#accessing-the-clsi",
---
+++ source/INDEX.md
@@ -0,0 +1,16 @@
+# Common LaTeX Service Interface
+The Common LaTeX Service Interface (CLSI) is an HTTP RESTful API for compiling LaTeX
+documents online.
+
+## How to get access
+To access the CLSI server you will need a developer token. To request a
+token, please email the server maintainer. 
+
+## How to report a missing package
+If the CLSI maintainer decided to run a striped-down version of LaTeX,
+it is possible that some packages are not installed by default. Please
+contact the CSLI maintainer for more information regarding missing
+packages.
+## Client
+If you are looking for a client to access this CLSI server,
+check out the [Remote LaTeX](http://github.com/pacbard/RLatex) client.
